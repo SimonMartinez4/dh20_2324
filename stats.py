4448ba19ff1f21fdc825a711532ba478c6a619bc
Simon Martinez <simonmartinez4@gmail.com>
2024-09-05 11:03:34 +0200

    version without custom graph stats

--- stats.py
+++ stats.py
@@ -26,8 +26,8 @@ def run():
         st.session_state.player = None
     if 'season_type' not in st.session_state:
         st.session_state.season_type = None
-    if 'stat_choice' not in st.session_state:
-        st.session_state.stat_choice = None
+    #if 'stat_choice' not in st.session_state:
+    #    st.session_state.stat_choice = None
 
     st.header("Stats DH20 2023-24")
 
@@ -66,16 +66,16 @@ def run():
             st.header("Efficacité au rebond")
             reb_graph(player,season_type)
             
-            st.header("Statistiques de base")
-            stat_options=['PTS', 'REB', 'AST', 'STL', 'BLK', 'FGM','FGA', 'FG_PCT', 'FG3M', 'FG3A', 'FG3_PCT', 'FTM', 'FTA', 'FT_PCT',
-                          'OREB', 'DREB', 'TOV', 'BLKA', 'PF', 'PFD', 'PLUS_MINUS', 'W_PCT',
-                          'CONTESTED_SHOTS', 'CONTESTED_SHOTS_2PT','CONTESTED_SHOTS_3PT', 'DEFLECTIONS', 'CHARGES_DRAWN',
-                          'SCREEN_ASSISTS','SCREEN_AST_PTS', 'OFF_LOOSE_BALLS_RECOVERED','DEF_LOOSE_BALLS_RECOVERED',
-                          'LOOSE_BALLS_RECOVERED','PCT_LOOSE_BALLS_RECOVERED_OFF', 'PCT_LOOSE_BALLS_RECOVERED_DEF',
-                          'OFF_BOXOUTS', 'DEF_BOXOUTS', 'BOX_OUTS', 'BOX_OUT_PLAYER_TEAM_REBS','BOX_OUT_PLAYER_REBS',
-                          'PCT_BOX_OUTS_OFF', 'PCT_BOX_OUTS_DEF','PCT_BOX_OUTS_TEAM_REB', 'PCT_BOX_OUTS_REB']
-            stat_choice = st.selectbox('Rubrique statistique', stat_options)
-            custom_graph(player,season_type,stat_choice)
+            #st.header("Statistiques de base")
+            #stat_options=['PTS', 'REB', 'AST', 'STL', 'BLK', 'FGM','FGA', 'FG_PCT', 'FG3M', 'FG3A', 'FG3_PCT', 'FTM', 'FTA', 'FT_PCT',
+            #              'OREB', 'DREB', 'TOV', 'BLKA', 'PF', 'PFD', 'PLUS_MINUS', 'W_PCT',
+            #              'CONTESTED_SHOTS', 'CONTESTED_SHOTS_2PT','CONTESTED_SHOTS_3PT', 'DEFLECTIONS', 'CHARGES_DRAWN',
+            #              'SCREEN_ASSISTS','SCREEN_AST_PTS', 'OFF_LOOSE_BALLS_RECOVERED','DEF_LOOSE_BALLS_RECOVERED',
+            #              'LOOSE_BALLS_RECOVERED','PCT_LOOSE_BALLS_RECOVERED_OFF', 'PCT_LOOSE_BALLS_RECOVERED_DEF',
+            #              'OFF_BOXOUTS', 'DEF_BOXOUTS', 'BOX_OUTS', 'BOX_OUT_PLAYER_TEAM_REBS','BOX_OUT_PLAYER_REBS',
+            #              'PCT_BOX_OUTS_OFF', 'PCT_BOX_OUTS_DEF','PCT_BOX_OUTS_TEAM_REB', 'PCT_BOX_OUTS_REB']
+            #stat_choice = st.selectbox('Rubrique statistique', stat_options)
+            #custom_graph(player,season_type,stat_choice)
 
         except TypeError:
             st.error(f"{player} n'a pas joué en {season_type} en 2023-2024.")
